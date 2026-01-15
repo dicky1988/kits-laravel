0f7772ee04aa3e3f86e90b407adb6336598438c4
dicky satria utama <dicky.satria.utama1988@gmail.com>
2026-01-15 12:45:30 +0700

    Update index.blade.php

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid px-3">
 
-        <div class="card shadow-sm mt-5">
+        <div class="card shadow-sm mt-3">
 
             <div class="card-header bg-white border-bottom">
                 <div class="d-flex justify-content-between align-items-center">
